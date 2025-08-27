931c25211b681e46605bb572bc3b320e6c6ffb97
“tut08455” <tut08455@temple.edu>
2025-08-27 13:35:42 -0400

    importing random string and adjusting formatting slightly

--- print_random_string.c
+++ print_random_string.c
@@ -1,35 +1,29 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
+#include "random.c"
 
 // random string generator wrapper function
 char* rand_string_alloc(size_t size)
 {
      char *s = malloc(size + 1);
      if (s) {
-         rand_string(s, size); // to be developed by the student
+        rand_string(s, size); // to be developed by the student
      }
      return s;
 }
 
-
-
 int main()
-
 {
     char *str;
-    size_t size =   ;  //specify a value for size of word
+    size_t size = 10;  //specify a value for size of word
 
 	/* seed the randomizer */
 	srand( (unsigned)time(NULL) );
 
 	printf("Today's random word: ");
     str = rand_string_alloc(size );
-    printf("%s",str);
+    printf("%s\n",str);
     free (str);
     
 
