e0a22f0a41a2050de6eb09faf7b94798075b4bc8
“tut08455” <tut08455@temple.edu>
2025-08-27 13:44:25 -0400

    added small comment in random.c

--- random.c
+++ random.c
@@ -4,12 +4,13 @@
 
 char* rand_string(char * output, size_t size) 
 {
+    // I do not check size of output. Possible memory leak if improperly used.
     char letters[] = "abcdefghijklmnopqrstuvwxyz"; 
     for(int i = 0; i < size; i++) {
         size_t index = (double) rand() / RAND_MAX * (sizeof letters - 1);
         *output = letters[index];
         
-        // Increment Pointer and Decrease remaining
+        // Incremenet pointer
         output++;
     }
 }
